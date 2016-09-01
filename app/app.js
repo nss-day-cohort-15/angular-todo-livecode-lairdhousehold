5df34b2e799a6e53db48efd88d626025f188f743
Jammy Laird <jammylaird@yahoo.com>
2016-09-01 07:31:21 -0500

    jammy1

--- app/app.js
+++ app/app.js
@@ -1,34 +1,15 @@
 "use strict";
-
-$scope.items = [
-  {
-    id: 0,
-    task: "mow the lawn",
-    isCompleted: false,
-    dueDate: "12/5/17",
-    assignedTo: "Greg",
-    location: "Joe's house",
-    urgency: "low",
-    dependencies: "sunshine, clippers, hat, water, headphones"
-  },
-  {
-    id: 1,
-    task: "grade quizzes",
-    isCompleted: false,
-    dueDate: "12/5/15",
-    assignedTo: "Christina",
-    location: "NSS",
-    urgency: "high",
-    dependencies: "wifi, tissues, vodka"
-  },
-  {
-    id: 2,
-    task: "take a nap",
-    isCompleted: false,
-    dueDate: "5/21/16",
-    assignedTo: "Joe",
-    location: "Porch of lakefront cabin",
-    urgency: "medium",
-    dependencies: "hammock, silence"
-  }
-];
+// module and array of dependencies
+var app = angular.module("TodoApp", ["ngRoute"]);
+app.config(function($routeProvider){
+    $routeProvider.
+        when ('/items/list', {
+            templateUrl:'partials/items-list.html',
+            controller :'TodoCtrl'
+        }).
+        when('items/new', {
+            templateUrl:'partials/items-form.html',
+            controller: 'TodoCtrl'
+        }).
+        otherwise ('items/list');
+});
---
+++ app/controllers/NavCtrl.js
@@ -0,0 +1,8 @@
+"use strict";
+app.controller("NavCtrl", function($scope){
+    $scope.navItems =[
+    {name: "Logout"},
+    {name: "All Items"},
+    {name: "New Items"}
+    ];
+});
